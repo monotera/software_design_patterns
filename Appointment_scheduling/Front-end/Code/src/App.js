ef57eaa556f058eff5802446069d6b7d91b0b912
Dacaramo <dankramirez@outlook.com>
2022-01-31 23:22:43 -0500

    React project created

---
+++ Appointment_scheduling/Front-end/Code/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"; //Importar bootstrap
+import Menu from "./components/Menu";
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
